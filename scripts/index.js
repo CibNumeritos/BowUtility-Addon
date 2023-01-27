c25f34d6c45d5ff78793255272d641fa876925cd
CibNumeritos <85264290+CibNumeritos@users.noreply.github.com>
2023-01-26 20:29:43 -0500

    Update index.js
    Prevent the ding sound being player with snowballs, splash potions, or eggs,
    Added a way to blacklist your own projectiles that won't trigger the ding sound!

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,13 @@
 import { world } from "@minecraft/server";
+// You can blacklist another projectiles by adding new strings here (eg: "ag:splash_health_potion") ;).
+const BlackListedProjectiles = [
+    "minecraft:snowball",
+    "minecraft:potion",
+    "minecraft:splash_potion",
+    "minecraft:egg"
+]; 
 world.events.projectileHit.subscribe((arg) => {
-    if (arg.entityHit?.entity.typeId != "minecraft:player" || arg.source.typeId != "minecraft:player" || arg.entityHit?.entity.name == arg.source.name) {
+    if (arg.entityHit?.entity.typeId != "minecraft:player" || arg.source.typeId != "minecraft:player" || arg.entityHit?.entity.name == arg.source.name || BlackListedProjectiles.includes(arg.projectile.typeId)) {
         return;
     };
     arg.source.playSound("random.orb", { volume: 0.5, pitch: 0.5 });
