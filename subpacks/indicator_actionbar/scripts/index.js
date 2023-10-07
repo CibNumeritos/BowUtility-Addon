5f4fa4faf8f35e9b330188bfa46279f66cddf6a8
CibNumeritos <imcib2498.212244.2808@gmail.com>
2023-10-07 12:36:12 -0500

    asd

--- subpacks/indicator_actionbar/scripts/index.js
+++ subpacks/indicator_actionbar/scripts/index.js
@@ -1,5 +1,4 @@
 import { MinecraftEffectTypes, world } from "@minecraft/server";
-// You can blacklist another projectiles by adding new strings here (eg: "ag:splash_health_potion") ;).
 const AllowedProjectiles = ["minecraft:arrow", "minecraft:thrown_trident"];
 world.afterEvents.projectileHitEntity.subscribe((arg) => {
   const hitEntity = arg.getEntityHit()?.entity;
@@ -12,11 +11,11 @@ world.afterEvents.projectileHitEntity.subscribe((arg) => {
   ) {
     return;
   }
-  source.runCommand("playsound random.orb ~ ~ ~ @s 1 0.5 1 ");
+  source.runCommand("playsound random.orb @s ~ ~ ~ 1 0.5 1 ");
   const health = hitEntity.getComponent("minecraft:health");
   source.runCommand(
     `titleraw @s actionbar {"rawtext":[{"text":"§e${hitEntity.name} is now at ${
-      health.current < 0 ? 0 : (health.current / 2).toFixed(1)
-    }/${Math.floor(health.value / 2)} §lHP"}]}`
+      health.currentValue < 0 ? 0 : (health.currentValue / 2).toFixed(1)
+    }/${Math.floor(health.defaultValue / 2)} §lHP"}]}`
   );
 });
--- subpacks/only_sound/scripts/index.js
+++ subpacks/only_sound/scripts/index.js
@@ -1,5 +1,4 @@
 import { world } from "@minecraft/server";
-// You can blacklist another projectiles by adding new strings here (eg: "ag:splash_health_potion") ;).
 const AllowedProjectiles = ["minecraft:arrow", "minecraft:thrown_trident"];
 world.afterEvents.projectileHitEntity.subscribe((arg) => {
   const hitEntity = arg.getEntityHit()?.entity;
@@ -12,5 +11,5 @@ world.afterEvents.projectileHitEntity.subscribe((arg) => {
   ) {
     return;
   }
-  source.runCommand("playsound random.orb ~ ~ ~ @s 1 0.5 1 ");
+  source.runCommand("playsound random.orb @s ~ ~ ~ 1 0.5 1 ");
 });
